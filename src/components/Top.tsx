702d2d3aaeb5ad109ec4ba39be3c93d3f366723f
controlcoder <ritikaryan2210@gmail.com>
2026-04-06 14:23:43 +0530

    add enter key press function

--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -5,25 +5,29 @@ type TopProps = {
 };
 
 export default function Top({ addItem }: TopProps) {
+  const handleSubmitItem = () => {
+    const inputVal = input && input.trim();
+    if (!inputVal) alert("Item value should not be input");
+    else addItem(inputVal);
+    setInput("");
+  };
+
   const [input, setInput] = useState("");
   return (
     <div className="flex items-center gap-5 border border-white p-6 rounded-lg text-black">
       <input
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleSubmitItem();
+          }
+        }}
         type="text"
         value={input}
         onChange={(e) => setInput(e.target.value)}
         className="bg-white outline-none input"
         placeholder="Add an item"
       />
-      <div
-        onClick={() => {
-          const inputVal = input && input.trim();
-          if (!inputVal) alert("Item value should not be input");
-          else addItem(inputVal);
-          setInput("");
-        }}
-        className="add btn text-4xl rounded bg-white"
-      >
+      <div onClick={() => handleSubmitItem()} className="add btn text-4xl rounded bg-white">
         <span>+</span>
       </div>
     </div>
